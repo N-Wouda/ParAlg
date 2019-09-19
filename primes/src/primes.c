90de8a153f95148488056873e092b3abca66c35b
Niels Wouda <nielswouda@gmail.com>
2019-09-19 20:32:58 +0200

    Further work on implementing a form of block sieving

--- primes/src/primes.c
+++ primes/src/primes.c
@@ -7,14 +7,15 @@
 
 int main()
 {
-    // Example usage
     size_t numPrimes = 0;
     bounds bounds = {0, 1000};
 
-    size_t *primes = sieve(&bounds, &numPrimes);
+    size_t const *primes = sieve(&bounds, &numPrimes);
 
     for (size_t idx = 0; idx != numPrimes; ++idx)
         printf("%zu\n", primes[idx]);
+
+    printf("Total number of primes: %zu\n", numPrimes);
 
     return EXIT_SUCCESS;
 }
--- primes/src/sieve.c
+++ primes/src/sieve.c
@@ -7,17 +7,27 @@
 #include "primes.h"
 
 
+bool *init(bounds const *bounds);
+
 size_t countPrimes(bool const *isPrime, size_t bound);
 
 size_t *getPrimes(bool const *isPrime, size_t bound, size_t numPrimes);
 
-// TODO: Parallel sieving probably requires us to consider a sieving method
-// TODO: that respects both bounds.
+/**
+ * Sieves prime numbers between the lowerBound and upperBound specified via the
+ * bounds parameter. The interval is [lowerBound, upperBound).
+ * 
+ * @param bounds    The lower and upper bound, as a struct.
+ * @param numPrimes After this function returns,numPrimes contains the number
+ *                  of primes in the returned array.
+ * @return          Array of primes, of length numPrimes.
+ */
 size_t *sieve(bounds const *bounds, size_t *numPrimes)
 {
+    // TODO: Parallel sieving probably requires us to consider a sieving method
+    // TODO: that respects both bounds.
     // Initially set all numbers to primes using a helper array.
-    bool *isPrime = malloc((bounds->upperBound + 1) * sizeof(bool));
-    memset(isPrime, true, (bounds->upperBound + 1) * sizeof(bool));
+    bool *isPrime = init(bounds);
 
     // TODO: this also sieves even numbers, but we can probably optimise those
     // TODO: away (and multiples of low primes, say, {3, 5, 7, 9}).
@@ -37,12 +47,22 @@ size_t *sieve(bounds const *bounds, size_t *numPrimes)
         }
     }
 
-    *numPrimes = countPrimes(isPrime, bounds->upperBound + 1);
+    *numPrimes = countPrimes(isPrime, bounds->upperBound);
     size_t *result = getPrimes(isPrime, bounds->upperBound, *numPrimes);
 
-    free(isPrime);      // clean-up helper array
+    free(isPrime);      // clean-up helper array.
 
     return result;
+}
+
+inline bool *init(bounds const *bounds)
+{
+    bool *isPrime = malloc(bounds->upperBound * sizeof(bool));
+
+    memset(isPrime, true, bounds->upperBound * sizeof(bool));
+    memset(isPrime, false, 2 * sizeof(bool));   // {0, 1} are not prime.
+
+    return isPrime;
 }
 
 inline size_t countPrimes(bool const *isPrime, size_t bound)
@@ -60,14 +80,14 @@ inline size_t *getPrimes(bool const *isPrime, size_t bound, size_t numPrimes)
     size_t *primes = malloc(numPrimes * sizeof(size_t));
     size_t counter = 0;
 
-    // Identifies the primes and inserts them into the primes list
+    // Identifies the primes and inserts them into the primes list.
     for (size_t number = 0; number != bound; ++number)
     {
         if (isPrime[number])
             primes[counter++] = number;
     }
 
-    assert(counter == numPrimes);  // sanity check
+    assert(counter == numPrimes);  // sanity check.
 
     return primes;
 }
