af9f8d83ad51ae4e5292a6f21bf2bb253120f72e
Niels Wouda <nielswouda@gmail.com>
2019-09-19 20:33:55 +0200

    Docstrings

--- primes/src/primes.h
+++ primes/src/primes.h
@@ -10,6 +10,15 @@ typedef struct bounds
     size_t upperBound;
 } bounds;
 
+/**
+ * Sieves prime numbers between the lowerBound and upperBound specified via the
+ * bounds parameter. The interval is [lowerBound, upperBound).
+ *
+ * @param bounds    The lower and upper bound, as a struct.
+ * @param numPrimes After this function returns,numPrimes contains the number
+ *                  of primes in the returned array.
+ * @return          Array of primes, of length numPrimes.
+ */
 size_t *sieve(bounds const *bounds, size_t *numPrimes);
 
 #endif // PRIMES_H
--- primes/src/sieve.c
+++ primes/src/sieve.c
@@ -13,19 +13,10 @@ size_t countPrimes(bool const *isPrime, size_t bound);
 
 size_t *getPrimes(bool const *isPrime, size_t bound, size_t numPrimes);
 
-/**
- * Sieves prime numbers between the lowerBound and upperBound specified via the
- * bounds parameter. The interval is [lowerBound, upperBound).
- * 
- * @param bounds    The lower and upper bound, as a struct.
- * @param numPrimes After this function returns,numPrimes contains the number
- *                  of primes in the returned array.
- * @return          Array of primes, of length numPrimes.
- */
+// TODO: Parallel sieving probably requires us to consider a sieving method
+// TODO: that respects both bounds.
 size_t *sieve(bounds const *bounds, size_t *numPrimes)
 {
-    // TODO: Parallel sieving probably requires us to consider a sieving method
-    // TODO: that respects both bounds.
     // Initially set all numbers to primes using a helper array.
     bool *isPrime = init(bounds);
 
