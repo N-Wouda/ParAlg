0e24037de9127082453c33cc2bb703b5b3e13e8f
Niels Wouda <nielswouda@gmail.com>
2019-09-25 11:01:26 +0200

    Valgrind fixes

--- primes/src/main.c
+++ primes/src/main.c
@@ -9,12 +9,14 @@ int main()
     size_t numPrimes = 0;
     size_t bound = 100;
 
-    size_t const *primes = sieve(bound, &numPrimes);
+    size_t *primes = sieve(bound, &numPrimes);
 
     for (size_t idx = 0; idx != numPrimes; ++idx)
         printf("%zu\n", primes[idx]);
 
     printf("Total number of primes: %zu\n", numPrimes);
+
+    free(primes);
 
     return EXIT_SUCCESS;
 }
