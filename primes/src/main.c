2535205a56b334d79d35b5c4296409988ac6fb93
Alex <alexandra.vegelien@gmail.com>
2019-09-27 09:50:49 +0200

    Merge branch 'master' of /Users/AlexandraVegelien/CLionProjects/ParAlg-Assignment with conflicts.

--- primes/src/main.c
+++ primes/src/main.c
@@ -1,12 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 #include "primes.h"
 
 int main()
 {
-    bounds const bounds = {0, 50};
+    size_t numPrimes = 0;
+    bounds bounds = {50, 100};
+    size_t *primes = boundedSieve(&bounds, &numPrimes);
 
-    bspSieve(&bounds, 4);
+    //bounds bounds = {0, 100};
+    //size_t *primes = sieve(&bounds, &numPrimes);
+    for (size_t idx = 0; idx != numPrimes; ++idx)
+        printf("%zu\n", primes[idx]);
+
+    printf("Total number of primes: %zu\n", numPrimes);
+
+    free(primes);
 
     return EXIT_SUCCESS;
 }
