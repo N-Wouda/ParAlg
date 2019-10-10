87cd47500cea2a3e6c01f6fdae0c34c7597075ca
Niels Wouda <nielswouda@gmail.com>
2019-10-10 13:11:48 +0200

    Fix some path stuff for Unity

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 # Installs Unity, our testing framework.
+cd ~/
 git clone "https://github.com/ThrowTheSwitch/Unity.git"
-git pull
-
-ls
-
-cp "src/*.h" "/usr/local/include"
+cp "Unity/src/*.h" "/usr/local/include"
