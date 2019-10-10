48e5882c3940d0b4c7e0c97984492777b38904b5
Niels Wouda <nielswouda@gmail.com>
2019-10-10 13:27:50 +0200

    Add export target cmake, info install script

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -3,4 +3,14 @@
 # Installs Unity, our testing framework.
 cd ~/
 git clone "https://github.com/ThrowTheSwitch/Unity.git"
-sudo cp Unity/src/*.h -t /usr/include
+
+cmake Unity
+
+echo "ls Current"
+ls
+
+echo "ls Unity"
+ls Unity
+
+sudo cp bin/*.h /usr/include
+sudo cp lib/*.a /usr/lib
