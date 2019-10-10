c1cd721b84fc01c71da096075e9620242c016578
Niels Wouda <nielswouda@gmail.com>
2019-10-10 13:56:37 +0200

    Unity make and copy files

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -3,8 +3,9 @@
 # Installs Unity, our testing framework.
 cd ~/
 git clone "https://github.com/ThrowTheSwitch/Unity.git"
+cmake Unity && make
 
-cmake Unity
-
-sudo make
-sudo make install
+# Unity's `make install` installs this stuff in an odd place, so better we just
+# do this ourselves.
+cp Unity/src/*.h -t /usr/include
+cp src/libunity.a -t /usr/lib
