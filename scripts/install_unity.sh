96ca6a99ff91eed45a477ddd8bbcc5adbc0ad17a
Niels Wouda <nielswouda@gmail.com>
2019-10-10 13:33:16 +0200

    Make and make install

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -6,11 +6,4 @@ git clone "https://github.com/ThrowTheSwitch/Unity.git"
 
 cmake Unity
 
-echo "ls Current"
-ls
-
-echo "ls Unity"
-ls Unity
-
-sudo cp bin/*.h /usr/include
-sudo cp lib/*.a /usr/lib
+sudo make && make install
