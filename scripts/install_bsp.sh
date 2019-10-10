2f4e33e5d2cc190c75db3a2a3be9ba7b729a9e7a
Niels Wouda <nielswouda@gmail.com>
2019-10-10 12:41:40 +0200

    Set bash to execute script

--- scripts/install_bsp.sh
+++ scripts/install_bsp.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# Installs Multicore BSP for C in the home directory. This is also where
+# CMake looks for it, so it should all work out nicely.
 cd ~/
 wget "http://www.multicorebsp.com/downloads/c/2.0.4/MulticoreBSP-for-C.tar.xz"
 tar xvfJ "MulticoreBSP-for-C.tar.xz"
