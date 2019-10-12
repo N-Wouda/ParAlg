8ea75935bcbfe97f73b868dd785cd569d4a89117
Niels Wouda <nielswouda@gmail.com>
2019-10-12 13:47:27 +0200

    Remove symlink

--- scripts/install_oclint.sh
+++ scripts/install_oclint.sh
@@ -7,9 +7,5 @@ curl -OL https://github.com/oclint/oclint/releases/download/v0.13/oclint-0.13-x8
 mkdir "${TRAVIS_BUILD_DIR}"/oclint
 tar xf oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz -C  "${TRAVIS_BUILD_DIR}"/oclint --strip-components=1
 
-# Eases access from other scripts.
-sudo ln -s "${TRAVIS_BUILD_DIR}"/oclint/bin/oclint /home/travis/bin/oclint
-sudo ln -s "${TRAVIS_BUILD_DIR}"/oclint/bin/oclint-json-compilation-database /home/travis/bin/oclint-json-compilation-database
-
 # Just so we have a log entry for this.
-oclint -version
+"${TRAVIS_BUILD_DIR}"/oclint/bin/oclint -version
