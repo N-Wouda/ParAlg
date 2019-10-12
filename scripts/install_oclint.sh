2fed1cb2408bd916ccb2ec0295c672ed29cd7058
Niels Wouda <nielswouda@gmail.com>
2019-10-12 13:30:54 +0200

    Directly link to OCLint

--- scripts/install_oclint.sh
+++ scripts/install_oclint.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-# Gets and installs OCLint 0.13 in the home directory
+# Gets and installs OCLint 0.13 in the home directory.
 curl -OL https://github.com/oclint/oclint/releases/download/v0.13/oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz
 
-mkdir ~/oclint
+mkdir "${TRAVIS_BUILD_DIR}"/oclint
 
 tar xf oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz -C ~/oclint --strip-components=1
-rm oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz
 
-export OCLINT_HOME="$HOME/oclint"
-export PATH="$OCLINT_HOME/bin:$PATH"
-
-oclint -version
+# Just so we have a log entry for this.
+~/oclint/bin/oclint -version
