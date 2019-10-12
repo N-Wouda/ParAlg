89b2eaadf31fff2f63f194f9fa422f8dfe947b33
Niels Wouda <nielswouda@gmail.com>
2019-10-12 13:40:13 +0200

    Clean-up a little bit

--- scripts/install_oclint.sh
+++ scripts/install_oclint.sh
@@ -3,9 +3,13 @@
 # Gets and installs OCLint 0.13 in the home directory.
 curl -OL https://github.com/oclint/oclint/releases/download/v0.13/oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz
 
+# Extract into dedicated folder.
 mkdir "${TRAVIS_BUILD_DIR}"/oclint
+tar xf oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz -C  "${TRAVIS_BUILD_DIR}"/oclint --strip-components=1
 
-tar xf oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz -C ~/oclint --strip-components=1
+# Eases access from other scripts.
+sudo ln -s "${TRAVIS_BUILD_DIR}"/oclint/bin/oclint /home/travis/bin/oclint
+sudo ln -s "${TRAVIS_BUILD_DIR}"/oclint/bin/oclint-json-compilation-database /home/travis/bin/oclint-json-compilation-database
 
 # Just so we have a log entry for this.
-"${TRAVIS_BUILD_DIR}"/oclint/bin/oclint -version
+oclint -version
