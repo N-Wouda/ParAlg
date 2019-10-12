a1ed6cc25efdd6e000b311737af4732b137afb0f
Niels Wouda <nielswouda@gmail.com>
2019-10-12 13:34:34 +0200

    More info

--- scripts/install_oclint.sh
+++ scripts/install_oclint.sh
@@ -7,5 +7,8 @@ mkdir "${TRAVIS_BUILD_DIR}"/oclint
 
 tar xf oclint-0.13-x86_64-linux-3.13.0-125-generic.tar.gz -C ~/oclint --strip-components=1
 
+ls ~/oclint
+ls ~/oclint/bin
+
 # Just so we have a log entry for this.
 ~/oclint/bin/oclint -version
