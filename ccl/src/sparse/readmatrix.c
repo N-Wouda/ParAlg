b77931aed7e324d988af6a442c421e32b7db80cd
Niels Wouda <nielswouda@gmail.com>
2019-12-04 15:12:34 +0100

    Use asserts and pessimistic status signalling

--- ccl/src/sparse/readmatrix.c
+++ ccl/src/sparse/readmatrix.c
@@ -9,41 +9,36 @@
 matrix_t readMatrix(char const *location, bool *status)
 {
     matrix_t mat = {NULL, NULL, NULL, 0};
+    *status = false;
 
     FILE *file = fopen(location, "r");
 
     if (file == NULL)
-    {
-        *status = false;
         return mat;
-    }
 
-    // TODO: this is a bit too large for a single function - should be factored
-    // into multiple smaller ones. Is 20 characters sufficient for a buffer?
-    char buffer[20];
+    // TODO: this is a bit too large for a single function - should perhaps be
+    // factored into multiple smaller ones.
+    char buffer[20];    // is this sufficient for a buffer?
     size_t idx = 0;
 
     while (fgets(buffer, sizeof(buffer), file) != NULL)
     {
-        if (buffer[strlen(buffer) - 1] != '\n')
-        {
-            printf("Read line %zu: insufficient buffer space.\n", idx);
-
-            *status = false;
-            return mat;
-        }
+        // The buffer size is insufficient.
+        assert(buffer[strlen(buffer) - 1] == '\n');
 
         char *line = buffer;
         int x, y, z;
 
         int numRead = sscanf(line, "%d %d %d", &x, &y, &z);
 
+        // First line contains only the number of non-zeroes; all others x y z
+        // triplets.
         assert(numRead == (idx == 0) ? 1 : 3);
 
         if (idx == 0)
         {
             // Only the first number was read (as there is just one), which is
-            // stored in 'x'. This is the number of non-zeroes.
+            // stored in 'x'.
             mat.length = x;
 
             mat.x = malloc(mat.length * sizeof(size_t));
