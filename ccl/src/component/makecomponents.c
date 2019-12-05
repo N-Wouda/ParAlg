bea4716c66805deecc160c1208bd34e59e62903a
Niels Wouda <nielswouda@gmail.com>
2019-12-05 16:57:17 +0100

    Some further clarifying comments

--- ccl/src/component/makecomponents.c
+++ ccl/src/component/makecomponents.c
@@ -2,7 +2,7 @@
 
 
 // TODO: this is the obvious, but slow O(n^2) implementation. This should be
-// made to run in *at most* O(n log n) using .
+// made to run in *at most* O(n log n) using e.g. binary search.
 void makeComponents(segment_t *segments, size_t numSegments)
 {
     for (size_t idx = 0; idx != numSegments; ++idx)
@@ -13,6 +13,7 @@ void makeComponents(segment_t *segments, size_t numSegments)
         {
             segment_t parSeg = segments[parentIdx];
 
+            // (x - 1, same y, matching z).
             if (parSeg.x == segment.x - 1 && parSeg.y == segment.y
                 && parSeg.zFirst <= segment.zFirst
                 && parSeg.zLast >= segment.zFirst)
@@ -21,6 +22,7 @@ void makeComponents(segment_t *segments, size_t numSegments)
                 break;
             }
 
+            // (same x, y - 1, matching z).
             if (parSeg.x == segment.x && parSeg.y == segment.y - 1
                 && parSeg.zFirst <= segment.zFirst
                 && parSeg.zLast >= segment.zFirst)
