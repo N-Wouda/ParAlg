10c798bff3cef46345ca65bc0255a6fab827fc7a
Niels Wouda <nielswouda@gmail.com>
2019-12-09 13:37:11 +0100

    Better describe one-pass method condition

--- ccl/src/component/findset.c
+++ ccl/src/component/findset.c
@@ -4,7 +4,7 @@
 segment *findSet(segment *candidate)
 {
     // One-pass path halving method. This ensures all nodes in the set point
-    // to the root node.
+    // to the root node - eventually.
     while (candidate->parent != candidate)
     {
         candidate->parent = candidate->parent->parent;
